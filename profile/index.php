d1793e4c8196600cf6b545e5cfc5c33852e7e8ec
Ahmad Nur Salim <ahmadnursalimktb@gmail.com>
2025-06-18 19:48:00 +0700

    menambahkan folder profile, index.php dan editProfile.php

---
+++ profile/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>profile | Page</title>
+    <link rel="stylesheet" href="../public/bootstrap/css/bootstrap.css">
+</head>
+
+<body>
+
+    <p>ini page profile</p>
+
+    <script src="../public/bootstrap/js/bootstrap.js"></script>
+</body>
+
+</html>
